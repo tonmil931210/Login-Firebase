13d4d0cc4e85b6e18de088c99fbe089ea5ae33ec
Milton Casanova G <milton931210@gmail.com>
2016-05-03 13:47:35 -0500

    second commit

--- app/src/main/java/com/example/asus/loginfirebase/Register.java
+++ app/src/main/java/com/example/asus/loginfirebase/Register.java
@@ -6,8 +6,12 @@ import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
@@ -40,22 +44,43 @@ public class Register extends AppCompatActivity {
 
     public void btnRegister(View view) throws UnsupportedEncodingException {
         edtRUser = (EditText) findViewById(R.id.edtRUser);
-        edtREmail = (EditText) findViewById(R.id.edtREmail);
-        edtRPassword = (EditText) findViewById(R.id.edtRPassword);
-        // YA CREO QUE ESTO LO CONOCES, AQUI SIMPLEMENTE ESTOY PUSH A LA DB CON LOS DATOS DEL USUARIO
-        Map<String, String> post1 = new HashMap<String, String>();
-        // EN LA DB SOLO TIENE EMAIL, CONTRASEÑA Y TYPE
-        // POR DEFECTO TODOS LOS USUARIOS LOS PONGO COMO USUARIO EN TYPE
-        // QUE SIGNIFICA QUE SON ESTUDIANTES
-        // LO PUEDES CAMBIAR COMO QUIERAS
-        post1.put("Email", edtREmail.getText().toString());
-        byte[] data = edtRPassword.getText().toString().getBytes("UTF-8");
-        String base64 = Base64.encodeToString(data, Base64.DEFAULT);
-        post1.put("Password", base64);
-        post1.put("Type", "Usuario");
+        rootUrl.child("Users/" + edtRUser.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {
+                    edtREmail = (EditText) findViewById(R.id.edtREmail);
+                    edtRPassword = (EditText) findViewById(R.id.edtRPassword);
+                    // YA CREO QUE ESTO LO CONOCES, AQUI SIMPLEMENTE ESTOY PUSH A LA DB CON LOS DATOS DEL USUARIO
+                    Map<String, String> post1 = new HashMap<String, String>();
+                    // EN LA DB SOLO TIENE EMAIL, CONTRASEÑA Y TYPE
+                    // POR DEFECTO TODOS LOS USUARIOS LOS PONGO COMO USUARIO EN TYPE
+                    // QUE SIGNIFICA QUE SON ESTUDIANTES
+                    // LO PUEDES CAMBIAR COMO QUIERAS
+                    post1.put("Email", edtREmail.getText().toString());
+                    byte[] data = new byte[0];
+                    try {
+                        data = edtRPassword.getText().toString().getBytes("UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+                    post1.put("Password", base64);
+                    post1.put("Type", "Usuario");
 
-        rootUrl.child("Users").child(edtRUser.getText().toString()).setValue(post1);
-        finish();
+                    rootUrl.child("Users").child(edtRUser.getText().toString()).setValue(post1);
+                    finish();
+                } else {
+                    Toast toast1 = Toast.makeText(getApplicationContext(), "ya existe user", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+
     }
 
     public void btnLogout(View view) {
@@ -66,4 +91,5 @@ public class Register extends AppCompatActivity {
         tvUserType.setText(currentUser.getType());
 
     }
+
 }
